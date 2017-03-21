05f4e9379f8611f43cb1c5845313d32ac2f90bb8
Harry ByungDeok Kim <byungdeok.kim@gmail.com>
2017-03-21 22:49:58 +0900

    good

---
+++ buf.h
@@ -0,0 +1,7 @@
+
+char *init_buffer();
+int save_data(char *data, int len);
+int get_data(char *p, int len);
+char *get_buf_ptr();
+int get_buf_size();
+int get_data_cnt();
---
+++ sdl.h
@@ -0,0 +1,3 @@
+
+
+void sdl_main();
